eaa65df0be2384c48e8d0ac06ecfd9bbe41b057d
eduardo <eduardooost@gmail.com>
2025-04-18 10:06:03 -0400

    test deploy

--- src/components/Navigation/Navigation001.tsx
+++ src/components/Navigation/Navigation001.tsx
@@ -2,45 +2,60 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navigation001.css";
 
-function Navigation001() {
-  const [sessionTrayIsExpanded, setsessionTrayIsExpanded] = useState(false);
+function Navigation001()
+{
+   const [sessionTrayIsExpanded, setsessionTrayIsExpanded] = useState(false);
+   const [navigationClasses, setnavigationClasses] = useState("Navigation001 Navigation001Visible");
 
-  const handleSessionButtonClick = () => {
-    setsessionTrayIsExpanded(!sessionTrayIsExpanded);
-  };
+   const handleSessionButtonClick = () =>
+   {
+      setsessionTrayIsExpanded(!sessionTrayIsExpanded);
+      setnavigationClasses((prev) =>
+      {
+         if (prev === "Navigation001 Navigation001Visible")
+         {
+            return "Navigation001 Navigation001Hidden"
+         }
+         else
+         {
+            return "Navigation001 Navigation001Visible"
+         }
+      });
+   };
 
-  useEffect(() => {
-    console.log("sessionTrayIsExpanded:", sessionTrayIsExpanded);
-  }, [sessionTrayIsExpanded]);
+   useEffect(() =>
+   {
+      console.log("sessionTrayIsExpanded:", sessionTrayIsExpanded);
+   }, [sessionTrayIsExpanded]);
 
-  return (
-    <nav className="Navigation001">
-      <ul className="Navigation001Ul">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <button type="button" onClick={handleSessionButtonClick}>
-          Session
-        </button>
-        {sessionTrayIsExpanded && (
-          <div>
+   return (
+      <nav className={navigationClasses}>
+         <ul className="Navigation001Ul">
             <li>
-              <Link to="/login">LogIn</Link>
+               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/singin">SingIn</Link>
+               <Link to="/contact">Contact</Link>
             </li>
-            <li>
-              <Link to="/logout">LogOut</Link>
-            </li>
-          </div>
-        )}
-      </ul>
-    </nav>
-  );
+            <button type="button" onClick={handleSessionButtonClick}>
+               Session
+            </button>
+            {sessionTrayIsExpanded && (
+               <div>
+                  <li>
+                     <Link to="/login">LogIn</Link>
+                  </li>
+                  <li>
+                     <Link to="/singin">SingIn</Link>
+                  </li>
+                  <li>
+                     <Link to="/logout">LogOut</Link>
+                  </li>
+               </div>
+            )}
+         </ul>
+      </nav>
+   );
 }
 
 export default Navigation001;
