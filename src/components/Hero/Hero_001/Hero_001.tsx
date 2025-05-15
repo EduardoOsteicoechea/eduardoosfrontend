e4f7bc478f0c043792c197573b296d2a3430147b
eduar <eduar@EDUARDO>
2025-05-15 17:18:40 -0400

    test deploy

--- src/components/Hero/Hero_001/Hero_001.tsx
+++ src/components/Hero/Hero_001/Hero_001.tsx
@@ -16,7 +16,7 @@ function Hero_001() {
 
       <div className="hero_heading_container">
         <h1 className="hero_heading_container_heading">
-          Multitask Role
+          Ethical Thinker - BIM Architect - Fullstack Software Developer
         </h1>
       </div>
       
